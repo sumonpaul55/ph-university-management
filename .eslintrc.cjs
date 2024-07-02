619597dd41c80c434312dccf3e5664e111e322c9
sumonpaul55 <sumpaul100@gmail.com>
2024-07-03 05:16:42 +0600

    basic layout

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "unused-var": false
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import MainLayout from "./components/layout/MainLayout"
+
 function App() {
 
   return (
     <>
-      <h1>Ph University management</h1>
+      <MainLayout></MainLayout>
     </>
   )
 }
