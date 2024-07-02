51bb23a947c5ca67327a1b9314086ebe5caa38af
sumonpaul55 <sumpaul100@gmail.com>
2024-07-03 05:57:05 +0600

    basic route setup and so more

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,21 +1,22 @@
 import { Layout, Menu } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { createElement } from 'react';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-    (icon, index) => ({
-        key: String(index + 1),
-        icon: createElement(icon),
-        label: `nav ${index + 1}`,
-    }),
-);
+const items = [
+    {
+        key: 1,
+        label: "Dashboard"
+    },
+    {
+        key: 2,
+        label: "profile"
+    }
+];
 const MainLayout = () => {
 
 
     return (
-        <Layout>
+        <Layout style={{ height: "100vh" }}>
             <Sider
                 breakpoint="lg"
                 collapsedWidth="0"
@@ -26,11 +27,17 @@ const MainLayout = () => {
                     console.log(collapsed, type);
                 }}
             >
-                <div className="demo-logo-vertical" />
+                <div className="demo-logo-vertical">
+                    <h1 style={{ fontSize: "20px", color: "white", fontWeight: "500", textAlign: "center", padding: '10px', borderBottom: "1px solid gray" }}>University</h1>
+                </div>
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
             </Sider>
             <Layout>
-                <Header style={{ padding: 0, }} />
+                <Header style={{ paddingLeft: "20px", background: "darkblue", height: "3rem", display: "flex", alignItems: "center" }} >
+                    <h1 style={{ fontWeight: "bold", color: "white", fontSize: "20px", textAlign: 'center', flex: "1" }}>University Management</h1>
+                </Header>
+
+
                 <Content style={{ margin: '24px 16px 0' }}>
                     <div
                         style={{
@@ -42,7 +49,7 @@ const MainLayout = () => {
                     </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                    University Mangement ©{new Date().getFullYear()} Created Sumon Paul
                 </Footer>
             </Layout>
         </Layout>
